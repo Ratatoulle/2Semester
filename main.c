848a97f825cbacc8c418cf5462d0e7395a04d2d5
Ratatoulle <1vasilev.andrey@gmail.com>
2022-03-30 18:41:51 +0500

    add 2 tests

--- main.c
+++ main.c
@@ -6,10 +6,10 @@ int main()
 	FILE* book;
 	errno_t err;
 	char* filename = (char*)malloc(13 * sizeof(char));
-	printf_s("Please, enter the name of file needed to get sorted (max 13 characters): ");
+	printf_s("Please, enter the name of file needed to get sorted (max 12 characters): ");
 	gets_s(filename, 12);
 	err = fopen_s(&book, filename, "r");
-
+	qsort()
 	if (err == 0)
 		printf("File was successfully opened.\n");
 	else
@@ -17,8 +17,7 @@ int main()
 		printf("Couldn't open file.\n");
 		exit(1);
 	}
-		
-	_fcloseall();
+	int close = _fcloseall();
 	return 0;
 
 }
